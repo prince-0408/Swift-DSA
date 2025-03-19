3add52735ff64c6e382604572c8ab726f2038b80
Prince-0408 <princekuyadav5646@gmail.com>
2025-03-20 01:20:45 +0530

    Plusone(array)

---
+++ Plusone.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digits = digits
+        var carry = 1
+
+        for i in stride(from: digits.count - 1, through: 0, by: -1) {
+            let sum = digits[i] + carry
+            digits[i] = sum%10
+            carry = sum/10
+            if carry == 0{break}
+        }
+        if carry > 0 {
+            digits.insert(carry, at:0)
+        }
+        return digits
+    }
+}
